4bb19bcbe447270656d2245d56d5087fbe18e927
EsihleGubula <ST10519904@rcconnect.edu.za>
2026-04-01 14:46:43 +0200

    Update MainActivity.kt

--- app/src/main/java/com/esihle/mytest/MainActivity.kt
+++ app/src/main/java/com/esihle/mytest/MainActivity.kt
@@ -20,10 +20,14 @@ class MainActivity : AppCompatActivity() {
         val timeDay = findViewById<EditText>(R.id.editTextText)
         val resetButton = findViewById<Button>(R.id.button2)
 
+        //Checks whether the input follows the 24-hour format (HH:mm)
+
         fun isValidTime(input: String): Boolean {
             val timeRegex = Regex("^([01][0-9]|2[0-3]):[0-5][0-9]$")
             return timeRegex.matches(input)
         }
+
+        // Gets the appropriate Spark message based on the input time
 
         fun getSparks(input: String): String {
             val morning = LocalTime.parse("08:59")
@@ -33,30 +37,39 @@ class MainActivity : AppCompatActivity() {
             val dinner = LocalTime.parse("19:59")
             val afterDinner = LocalTime.parse("04:59")
 
+            // Parse the user input into a LocalTime object for easier comparison
+
             val userTime = LocalTime.parse(input)
 
-            return if (userTime.isBefore(morning)) {
+            // Check the userTime against set limits to decide which prompt to use
+
+            return if (userTime.isBefore(morning) || userTime.equals(morning)) {
                 "Send a \"Good morning\" text to a family member"
-            } else if (userTime.isAfter(morning) && userTime.isBefore(midMorning)) {
+            } else if (userTime.isAfter(morning) && (userTime.isBefore(midMorning)||userTime.equals(midMorning))) {
                 "Reach out to a colleague with a quick \"Thank you\" "
-            } else if (userTime.isAfter(midMorning) && userTime.isBefore(afternoon)) {
+            } else if (userTime.isAfter(midMorning) && (userTime.isBefore(afternoon)||userTime.equals(afternoon))) {
                 "Share a funny meme or interesting link with a friend "
-            } else if (userTime.isAfter(afternoon) && userTime.isBefore(afternoonSnack)) {
+            } else if (userTime.isAfter(afternoon) && (userTime.isBefore(afternoonSnack)||userTime.equals(afternoonSnack))) {
                 " Send a quick \" thinking of you\" "
-            } else if (userTime.isAfter(afternoonSnack) && userTime.isBefore(dinner)) {
+            } else if (userTime.isAfter(afternoonSnack) && (userTime.isBefore(dinner) ||userTime.equals(dinner))) {
                 "Call a friend or relative for a 5-minute catch up "
             } else {
                 " Leave a thoughtful comment on a friend's post"
             }
         }
+
+            // validates the user's time input and shows the corresponding Spark message if valid
+
             enterButton.setOnClickListener {
                 if (!isValidTime(timeDay.text.toString())) {
-                    timeDay.error = "enter your correct day time in correct format(HH:mm)"
+                    timeDay.error = "enter your correct day time in correct format (HH:mm)"
+                }else {
+                    results.setText(getSparks(timeDay.text.toString()))
                 }
 
-                results.setText(getSparks(timeDay.text.toString()))
-
             }
+
+            // Clears the input and result fields when the reset button is clicked .
 
             resetButton.setOnClickListener {
                 results.setText("")
